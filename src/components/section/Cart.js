e507012545d2682dcb4f41a62850d9c0ed6b3e34
chrisclarkedev <cclarke8904@gmail.com>
2021-09-22 14:32:12 -0400

    formated cart component to have products passed in when add to cart button is clicked

--- src/components/section/Cart.js
+++ src/components/section/Cart.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import { DataContext } from '../Context';
+import { Link } from 'react-router-dom';
+import Colors from './Colors';
+import '../styles/Details.css';
 
 export class Cart extends Component {
+  static contextType = DataContext;
+
   render() {
-    return <div>Cart Component</div>;
+    const { cart } = this.context;
+    return (
+      <>
+        {cart.map((item) => (
+          <div className="details" key={item._id}>
+            <img src={item.src} alt="" />
+            <div className="box">
+              <div className="row">
+                <h2>{item.title}</h2>
+                <span>{item.price}</span>
+              </div>
+              <Colors colors={item.colors} />
+              <p>{item.Brand}</p>
+              <p>{item.Description}</p>
+              <Link to="/cart" className="cart">
+                Add to cart
+              </Link>
+            </div>
+          </div>
+        ))}
+      </>
+    );
   }
 }
-
 export default Cart;
