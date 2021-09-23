46761687e3a029033efb1d718676f49d0c845e4b
chrisclarkedev <cclarke8904@gmail.com>
2021-09-23 14:27:56 -0400

    display of no products

--- src/components/section/Cart.js
+++ src/components/section/Cart.js
@@ -10,42 +10,46 @@ export class Cart extends Component {
 
   render() {
     const { cart, increase, reduction, removeProduct } = this.context;
-    return (
-      <>
-        {cart.map((item) => (
-          <div className="details cart" key={item._id}>
-            <img src={item.src} alt="" />
-            <div className="box">
-              <div className="row">
-                <h2>{item.title}</h2>
-                <span>{item.price}</span>
+    if (cart.length === 0) {
+      return <h2 style={{ textAlign: 'center' }}>No Products</h2>;
+    } else {
+      return (
+        <>
+          {cart.map((item) => (
+            <div className="details cart" key={item._id}>
+              <img src={item.src} alt="" />
+              <div className="box">
+                <div className="row">
+                  <h2>{item.title}</h2>
+                  <span>{item.price}</span>
+                </div>
+                <Colors colors={item.colors} />
+                <p>{item.Brand}</p>
+                <p>{item.Description}</p>
+                <div className="amount">
+                  <button className="count" onClick={() => reduction(item._id)}>
+                    {' '}
+                    -{' '}
+                  </button>
+                  <span>{item.count}</span>
+                  <button className="count" onClick={() => increase(item._id)}>
+                    {' '}
+                    +{' '}
+                  </button>
+                </div>
               </div>
-              <Colors colors={item.colors} />
-              <p>{item.Brand}</p>
-              <p>{item.Description}</p>
-              <div className="amount">
-                <button className="count" onClick={() => reduction(item._id)}>
-                  {' '}
-                  -{' '}
-                </button>
-                <span>{item.count}</span>
-                <button className="count" onClick={() => increase(item._id)}>
-                  {' '}
-                  +{' '}
-                </button>
+              <div className="delete" onClick={() => removeProduct(item._id)}>
+                X
               </div>
             </div>
-            <div className="delete" onClick={() => removeProduct(item._id)}>
-              X
-            </div>
+          ))}
+          <div className="total">
+            <Link to="/payment">Payment</Link>
+            <h3>Total: 0</h3>
           </div>
-        ))}
-        <div className="total">
-          <Link to="/payment">Payment</Link>
-          <h3>Total: 0</h3>
-        </div>
-      </>
-    );
+        </>
+      );
+    }
   }
 }
 export default Cart;
