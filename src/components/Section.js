6baeb61bed4c559a324b20e8fe4cd0f58e9489ba
chrisclarkedev <cclarke8904@gmail.com>
2021-09-28 15:12:06 -0400

    created home page component

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,12 +4,13 @@ import Details from './section/Details';
 import { Route } from 'react-router-dom';
 import Cart from './section/Cart';
 import Payment from './section/Payment';
+import Home from './section/Home';
 
 export class Section extends Component {
   render() {
     return (
       <section>
-        <Route path="/" component={Products} exact />
+        <Route path="/" component={Home} exact />
         <Route path="/product" component={Products} exact />
         <Route path="/product/:id" component={Details} />
         <Route path="/cart" component={Cart} />
---
+++ src/components/section/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the home page component</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
