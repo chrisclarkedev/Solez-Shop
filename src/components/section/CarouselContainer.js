5fcf72442bbf01aee30a22cbb0e335fa642bdd59
chrisclarkedev <cclarke8904@gmail.com>
2021-09-29 14:11:46 -0400

    changed interval consistency to 2 seconds

--- src/components/section/CarouselContainer.js
+++ src/components/section/CarouselContainer.js
@@ -7,21 +7,22 @@ import car4 from '../images/car4.jpg';
 const CarouselContainer = () => {
   return (
     <Carousel>
-      <Carousel.Item interval={1000}>
+      {/* INTERVAL IS 2 sec. */}
+      <Carousel.Item interval={2000}>
         <img className="d-block w-100" src={car1} alt="First slide" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
+      <Carousel.Item interval={2000}>
         <img className="d-block w-100" src={car4} alt="Second slide" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item interval={2000}>
         <img className="d-block w-100" src={car2} alt="Third slide" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
