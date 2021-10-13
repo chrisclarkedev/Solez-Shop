cc36e231f46a4c9d2fe364016c2c95d2066a07e9
chrisclarkedev <cclarke8904@gmail.com>
2021-10-13 17:11:41 -0400

    fixed styling issue of overlapping bootstrap row components

--- src/components/section/Login.js
+++ src/components/section/Login.js
@@ -4,22 +4,24 @@ import '../styles/Login.css';
 export class Login extends Component {
   render() {
     return (
-      <section class="login py-5 bg-light">
-        <div class="container">
-          <div class="row3 g-0">
-            <div className="col-lg-5">
-              <img
-                src={require('../images/login.jpg')}
-                class="img-fluid"
-                alt=""
-              />
-            </div>
-            <div className="col-lg-7">
-              <h1>Login</h1>
+      <>
+        <section class="login py-5 bg-light">
+          <div class="container">
+            <div class="row g-0">
+              <div class="col-lg-5">
+                <img
+                  src="https://i.ibb.co/zm1LtqQ/login.jpg"
+                  class="img-fluid"
+                  alt=""
+                ></img>
+              </div>
+              <div class="col-lg-7 text-center py-5">
+                <h1>LOGIN</h1>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </>
     );
   }
 }
