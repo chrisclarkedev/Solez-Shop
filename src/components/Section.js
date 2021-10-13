ad1be042c5bec3034db0160fbbfb97c585bfbc3c
chrisclarkedev <cclarke8904@gmail.com>
2021-10-13 16:11:34 -0400

    login component created

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,6 +6,7 @@ import Cart from './section/Cart';
 import Payment from './section/Payment';
 import Home from './section/Home';
 import Contact from './section/Contact';
+import Login from './section/Login';
 
 export class Section extends Component {
   render() {
@@ -14,6 +15,7 @@ export class Section extends Component {
         <Route path="/" component={Home} exact />
         <Route path="/product" component={Products} exact />
         <Route path="/contact" component={Contact} exact />
+        <Route path="/login" component={Login} exact />
         <Route path="/product/:id" component={Details} />
         <Route path="/cart" component={Cart} />
         <Route path="/payment" component={Payment} />
---
+++ src/components/section/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is login component</h1>
+      </div>
+    );
+  }
+}
+
+export default Login;
