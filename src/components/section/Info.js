5ddaa9e0538cdd143061287c8dec3d65e686538e
chrisclarkedev <cclarke8904@gmail.com>
2021-10-19 14:27:05 -0400

    added first set of image

--- src/components/section/Info.js
+++ src/components/section/Info.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import '../styles/Info.css';
+import img1 from '../images/top1_dunklowbw.jpg';
+import img2 from '../images/top2_j5oreo.jpg';
 
 export class Info extends Component {
   render() {
     return (
-      <div>
+      <>
         <h1>Top 10 Trending</h1>
-      </div>
+        <div class="container-fluid">
+          <div class="row8">
+            <div class="column">
+              <img src={img1} alt="" />
+              <img src={img2} alt="" />
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
