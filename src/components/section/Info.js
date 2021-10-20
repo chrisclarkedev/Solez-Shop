5a4a65b93f36b795e531d7ad61b39a04c1beca0e
chrisclarkedev <cclarke8904@gmail.com>
2021-10-20 13:31:58 -0400

    curved header in info section

--- src/components/section/Info.js
+++ src/components/section/Info.js
@@ -23,21 +23,21 @@ export class Info extends Component {
         <div class="container-fluid">
           <div class="row">
             <div class="column">
-              <img src={img1} alt="" />
+              <img src={img11} alt="" />
               <img src={img2} alt="" />
               <img src={img3} alt="" />
               <img src={img4} alt="" />
             </div>
             <div class="column">
-              <img src={img5} alt="" />
+              <img src={img8} alt="" />
               <img src={img6} alt="" />
               <img src={img7} alt="" />
-              <img src={img8} alt="" />
+              <img src={img5} alt="" />
             </div>
             <div class="column">
               <img src={img9} alt="" />
               <img src={img10} alt="" />
-              <img src={img11} alt="" />
+              <img src={img1} alt="" />
               <img src={img12} alt="" />
             </div>
           </div>
